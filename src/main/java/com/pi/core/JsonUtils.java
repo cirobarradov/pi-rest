b909401c673978cdced80491a2e5802fc9354418
Ricardo Bravo <rbravo@datiobd.com>
2017-08-29 17:31:49 +0200

    Add files via upload

--- src/main/java/com/pi/core/JsonUtils.java
+++ src/main/java/com/pi/core/JsonUtils.java
@@ -1,21 +1,23 @@
 package com.pi.core;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.pi.bean.Input;
 import com.pi.bean.Output;
 
 
 public class JsonUtils {
 
-    private static Gson gson = new Gson();
     public static Input getLists (String json)
     {
-
+        Gson gson = new Gson();
         return gson.fromJson(json, Input.class);
     }
 
     public static String formatOutput (String res)
     {
-        return gson.toJson(new Output(res));
+        Gson gson = new GsonBuilder().disableHtmlEscaping().create();
+        String result= gson.toJson(new Output(res));
+        return result;
     }
 }
